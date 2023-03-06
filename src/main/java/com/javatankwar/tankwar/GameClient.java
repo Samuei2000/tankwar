650dd43dc011487c4c5b9869f7a6e4214d8f2522
samuei2000 <kyohn9178@gmail.com>
2023-03-06 00:05:35 +0000

    显示主界面和我方坦克

---
+++ src/main/java/com/javatankwar/tankwar/GameClient.java
@@ -0,0 +1,29 @@
+package com.javatankwar.tankwar;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameClient extends JComponent {
+    public GameClient(){
+        this.setPreferredSize(new Dimension(800,600));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(),400,100,null);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        frame.setTitle("史上最无聊的坦克大战");
+        frame.setIconImage(new ImageIcon("assets/images/icon.png").getImage());
+        GameClient client = new GameClient();
+        client.repaint();
+        frame.add(client);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
